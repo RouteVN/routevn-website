22c4c10955f41cb3d650bba7278f8851bf33e01b
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2026-04-22 10:32:30 +0800

    Use local theme.css for creator docs (#66)

--- static/public/rtgl-icons.js
+++ static/public/rtgl-icons.js
@@ -71,5 +71,18 @@ window.rtglIcons = {
 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M6 15L12 9L18 15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
+  `,
+  menu: `
+<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4 7H20" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+<path d="M4 12H20" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+<path d="M4 17H20" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+</svg>
+  `,
+  close: `
+<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+<path d="M18 6L6 18" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+</svg>
   `
 }
